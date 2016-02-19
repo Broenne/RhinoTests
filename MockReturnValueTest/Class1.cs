cd6cf7c8891d577f6b364bb5dd68bcb56488a886
Broenne <marcus.broenstrup@gmail.com>
2016-02-19 20:47:51 +0100

    no message

---
+++ MockReturnValueTest/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace MockReturnValueTest
+{
+    
+    public class Class1
+    {
+        [Test]
+        public void TestLogger()
+        {
+            Debug.WriteLine("text");
+            ILogger logger = MockRepository.GenerateMock<ILogger>();
+            logger.Stub(x => x.LogDebug("ccc"))
+                .IgnoreArguments()
+                //.WhenCalled((vv)=>this.Helper(vv.Arguments[0].ToString()));
+                .WhenCalled((vv) => Debug.WriteLine(vv.Arguments[0].ToString()))
+                ;
+            var service = new Service(logger);
+            //logger.AssertWasCalled(x=>x.LogDebug(""),arg=>arg.IgnoreArguments());
+        }
+
+        void Helper(string text)
+        {
+            Debug.WriteLine(text);
+        }
+    }
+
+
+    public class Service
+    {
+        public Service(ILogger logger)
+        {
+            logger.LogDebug("log den error");
+        }
+        
+    }
+
+    public interface ILogger
+    {
+        void LogDebug(string xxx);
+    }
+
+}
